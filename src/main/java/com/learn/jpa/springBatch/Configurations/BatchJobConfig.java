5cfffb786ef70d3cb147fd16a313a14d14e0a1ed
roshnirajan1106 <roshnirajan8888@gmail.com>
2025-05-01 20:45:38 +0530

    added all the files for dep

--- src/main/java/com/learn/jpa/springBatch/Configurations/BatchJobConfig.java
+++ src/main/java/com/learn/jpa/springBatch/Configurations/BatchJobConfig.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
